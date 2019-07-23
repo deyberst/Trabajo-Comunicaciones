27f979dbe1a7a28950573ab9acffd75c4b2c540c
deyberst <deyberst@gmail.com>
2019-07-23 18:03:19 -0500

    Subiendo nuevo

--- public/chat.js
+++ public/chat.js
@@ -3,24 +3,39 @@ const socket = io()
 //Elementos del index.html
 let message = document.getElementById('message');
 let username = document.getElementById('username');
-let button = document.getElementById('send');
+let enviar = document.getElementById('send');
+let login = document.getElementById('login');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
-button.addEventListener('click' , function () {
+/*var product = null;
+
+function sub(){
+product = document.getElementsByName("color)[0].value;
+};*/
+
+enviar.addEventListener('click' , function () {
     socket.emit('chat:message' , {
         message : message.value,
         username: username.value
     });
 });
 
+login.addEventListener('click' , function () {
+
+});
+
 message.addEventListener('keypress', function () {
     socket.emit('chat:typing' , username.value);
 });
 
+function getRandomRGBPart () {
+    return Math.random() * (256 - 0);
+}
+
 socket.on('chat:message', function (data) {
     actions.innerHTML = '';
-    output.innerHTML += `<b style="color:#87;"> ${data.message}</b>`
+    output.innerHTML += `<b style="color:rgb(${getRandomRGBPart()},${getRandomRGBPart()},${getRandomRGBPart()});"> ${data.message}</b>`
 });
 
 socket.on('chat:typing', function (data){
